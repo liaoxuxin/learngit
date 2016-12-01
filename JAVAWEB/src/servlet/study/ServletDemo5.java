47a54d762f9aa7fddfa2ab145aec5c07c7c34e78
liaoxuxin <1164641296@qq.com>
2016-12-01 17:51:01 +0800

    javaweb1201

---
+++ JAVAWEB/src/servlet/study/ServletDemo5.java
@@ -0,0 +1,39 @@
+package servlet.study;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo5
+ */
+@WebServlet("/ServletDemo5")
+public class ServletDemo5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDemo5() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.getOutputStream().write("servletDemo5".getBytes());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    this.doGet(request, response);
+	}
+
+}
